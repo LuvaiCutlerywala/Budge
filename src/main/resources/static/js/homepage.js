b69547313d065078fd6c0bfa948b4b0ec1cd025c
Luvai Cutlerywala <luvaidc@gmail.com>
2022-07-14 09:35:26 +0100

    Reworking front-end for working as async

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -7,98 +7,18 @@ function displayBudgetManagementArea(){
     }
 }
 
+function updatePrompt(){
+
+}
+
 function addBudget(){
-    let prompt = document.getElementById("action-prompt");
-    if(postBudget()){
-        prompt.innerHTML = "Your budget was successfully added.";
-    } else {
-        prompt.innerHTML = "There was a fault in adding your budget.";
-    }
+
 }
 
 function removeBudget(){
-    let prompt = document.getElementById("action-prompt");
-    if(deleteBudget()){
-        prompt.innerHTML = "Your budget was successfully removed.";
-    } else {
-        prompt.innerHTML = "There was a fault in removing your budget.";
-    }
+
 }
 
 function viewBudgets(){
-    let prompt = document.getElementById("action-prompt");
-    let budgetDisplay = document.getElementById("budgets");
-
-    let request = new XMLHttpRequest();
-    request.open("GET", "http://192.168.50.154:8080/api/view-budgets", false);
-    request.send(null);
-
-    //response keeps returning as 
-    if(request.response !== null && request.response !== ""){
-        prompt.innerHTML = "Check the console";
-        budgetDisplay.innerHTML = request.response;
-        console.log(request.response);
-    } else {
-        prompt.innerHTML = "There was a problem fetching your budgets";
-    }
-}
-
-function postBudget(){
-    //Get the request obj and values for the budget title and amounts.
-    let request = new XMLHttpRequest();
-    let params = new URLSearchParams();
-    let title = document.getElementById("budget-title").value;
-    let amount = document.getElementById("budget-amount").value;
-    let status = 0;
-
-    //Load request with URL and send along with params.
-    request.open("POST", "http://192.168.50.154:8080/api/add-budget", false);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    params.append("title", title);
-    params.append("amount", amount);
-
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE){
-            status = request.status;
-        }
-    };
-
-    request.send(params);
-
-    if(status >= 200 && status < 300){
-        return true;
-    }
     
-    return false;
 }
-
-function deleteBudget(){
-    //Get the request obj and value for budget title.
-    let request = new XMLHttpRequest();
-    let title = document.getElementById("budget-title").value;
-    let params = new URLSearchParams();
-    
-    //Load request with URL and send along with params.
-    request.open("DELETE", "http://192.168.50.154:8080/api/remove-budget", false);
-    params.append("title", title);
-    request.send(params);
-
-    if(request.ok){
-        return true;
-    }
-
-    return false;
-}
-
-/*
-function getBudgets(){
-    //Get the request obj.
-    let request = new XMLHttpRequest();
-
-    //Load request with URL and send.
-    request.open("GET", "http://localhost:8080/api/view-budgets");
-    request.send(null);
-
-    return request;
-}
-*/
